e47dc588ff911091a25af9ae63815603e25b0dbe
Nitin Yadav <nitin.yadav@saavn.com>
2020-08-31 14:44:04 +0530

    All features added

--- BullsEye/BullsEye/ContentView.swift
+++ BullsEye/BullsEye/ContentView.swift
@@ -13,6 +13,8 @@ struct ContentView: View {
     @State var alertVisible = false
     @State var sliderValue = 50.0
     @State var target = Int.random(in: 1...100)
+    @State var score = 0
+    @State var currentRound = 1
     
     var body: some View {
         VStack {
@@ -36,29 +38,35 @@ struct ContentView: View {
             //Button Row
             Button(action: {
                 self.alertVisible = true
+                self.score += self.pointsForCurrentRound()
             }) {
                 Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/)
                     .fontWeight(.bold)
                     .foregroundColor(Color.black)
             }
             .alert(isPresented: $alertVisible) { () -> Alert in
-                return Alert(title: Text("Hola!"), message: Text("Slider Value is \(sliderValueRounded()).\n" +
+                return Alert(title: Text("\(alertMessage())"), message: Text("Slider Value is \(sliderValueRounded()).\n" +
                     "You scored \(self.pointsForCurrentRound()) points in this round"
-                ), dismissButton: .default(Text("Dismiss!")))
+                ), dismissButton: .default(Text("Play Again!")){
+                    self.target = Int.random(in: 1...100)
+                    self.currentRound += 1
+                    })
             }
             Spacer()
             
             //Score Row
             HStack{
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Button(action: {
+                    self.newGame()
+                }) {
                    Text("Start Over")
                 }
                 Spacer()
                 Text("Score:")
-                Text("999999")
+                Text("\(score)")
                 Spacer()
                 Text("Round:")
-                Text("999")
+                Text("\(currentRound)")
                 Spacer()
                 Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
                     Text("Info")
@@ -68,12 +76,47 @@ struct ContentView: View {
         }
     }
     
+    
     func sliderValueRounded() -> Int {
         Int(self.sliderValue.rounded())
     }
     
     func pointsForCurrentRound()-> Int{
-        100 - abs(sliderValueRounded() - self.target)
+        let maxScore = 100
+        let difference = abs(sliderValueRounded() - self.target)
+        let bonus : Int
+        if difference == 0{
+            bonus = 100
+        }
+        else if difference <= 3{
+            bonus = 50
+        }
+        else{
+            bonus = 0
+        }
+        return maxScore - difference + bonus
+    }
+    
+    func newGame(){
+        currentRound = 1
+        score = 0
+        target = Int.random(in: 1...100)
+        sliderValue = 50.0
+    }
+    
+    func alertMessage() -> String{
+        let difference = abs(sliderValueRounded() - self.target)
+        let title : String
+        if difference == 0{
+            title = "Perfect!"
+        }
+        else if difference <= 10{
+            title = "You were close!"
+        }
+        else{
+            title = "Are You even Trying?"
+        }
+        return title
     }
     
 }
